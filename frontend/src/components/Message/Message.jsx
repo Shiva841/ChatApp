69c5bfd0f995db7f0666d913578868f2ade526b8
Groot-2001 <shivasilmawala841@gmail.com>
2022-07-02 17:24:02 +0530

    new chat send feature added

--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -5,7 +5,7 @@ export default function Message({message,own}) {
   return (
     <div className={own?"Message own":"Message"}>
       <div className="message-top">
-        <img src="https://images.unsplash.com/photo-1594007759138-855170ec8dc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80"
+        <img src={message.profilepicture?message.profilepicture:"https://images.unsplash.com/photo-1594007759138-855170ec8dc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80"}
          alt="" 
          className="message-img" />
          <p className="message-text">{message.text}</p>
